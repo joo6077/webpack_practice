c166aaa82a1b54167c8cf6102bf6e2ba9ac637da
weedskimhj <kimhj@weedscomm.com>
2021-04-20 18:16:27 +0900

    loader 쪽 보는중..

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,21 @@ const path = require('path')
 
 module.exports = {
     mode: 'development',
-    entry: './src/app.js',
+    entry: {
+        main: './src/app.js',
+    },
     output: {
        path: path.resolve(__dirname, 'dist'),
-       filename: 'bundle.js'
+       filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    path.resolve('./my-webpack-loader.js')
+                ]
+             }
+        ]
     }
 }
